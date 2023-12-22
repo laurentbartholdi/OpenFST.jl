85804beeb7985da02d1a221ac4e5d50c59a07307
Laurent Bartholdi <laurent.bartholdi@gmail.com>
2023-12-22 11:36:25 +0100

    put back compilation

--- deps/build.jl
+++ deps/build.jl
@@ -5,7 +5,7 @@ openfstdir = "$(@__DIR__)/$openfstver/"
 
 run(`tar xfz $(@__DIR__)/$openfstver.tar.gz -C $(@__DIR__)`)
 run(pipeline(openfstver*".patch",`patch -p0`))
-false && cd(openfstdir) do
+cd(openfstdir) do
     run(`./configure --prefix=$(@__DIR__)`)
     run(`make -j install`)
 end
